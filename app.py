29a0a3c9e3e3a35e8b23cae64f4d783d6e07f500
Russell <russell.treagus@hotmail.co.uk>
2018-10-01 22:32:35 +0000

    main display box for riddles added. username being passed through from login page and started logging users in a txt file

--- app.py
+++ app.py
@@ -3,11 +3,37 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 
 app = Flask(__name__)
 
-@app.route ('/')
+def write_to_file(filename, data):
+    """ handle process of writing to file """
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+
+
+
+# <-------------------- LOGIN PAGE ---------------------->
+@app.route("/")
+@app.route ("/", methods=["POST", "GET"])
 def index():
     
+    if request.method == "POST":
+        username = request.form["username"]
+        write_to_file("data/users.txt", username + "\n")
+         
+         
+        return render_template("riddle.html", username=username)
     return render_template("index.html")
     
+
+
+# <---------------------- MAIN PAGE ------------------------->
+
+@app.route('/<username>/riddle/', methods=["POST", "GET"])
+def riddle():
+
+  
+    
+    return render_template("riddle.html")
 
 
 
